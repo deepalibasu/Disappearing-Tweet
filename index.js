91c5991939c43a70a5dfa58fd066527fe8ddca54
deepalibasu <71073450+deepalibasu@users.noreply.github.com>
2020-09-10 19:10:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <>
+     <div>
+      <h1> TWITTER</h1>
+      <input
+       type="text"
+       placeholder="Enter your Tweet"
+      />
+      <button>CLICK ME</button>
+      </div>
+    </>,document.getElementById('root')
+   );
